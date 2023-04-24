1cf6b3bd77560c6085dd446269ec03e91d62b1e3
Oliwer <oliwerl@outlook.com>
2023-04-24 14:48:59 +0200

    Picture uploaded to screen

--- JS/home.js
+++ JS/home.js
@@ -3,13 +3,19 @@ const api_key = "d7eBBdpVdN08nChtJhFZzudXealrUpI6Xz0FsfuK0d5klpBSt6XzL2Zm";
 const headers = {
   authorization: api_key,
 };
-fetch("https://api.pexels.com/v1/photos/2014422", { headers })
-  .then((response) => response.json)
-  .then((r) => console.log(r));
 
-const div_dom = document.createElement("div");
+async function getPhoto() {
+  const response = await fetch("https://api.pexels.com/v1/photos/2014422", {
+    headers,
+  });
+  const resource = await response.json();
+  console.log(resource);
+  const div_dom = document.createElement("div");
 
-div_dom.innerHTML = `
-<img src="${r.url}">
-`;
-document.querySelector("body").append(div_dom);
+  div_dom.innerHTML = `
+      <p>${resource.id}</p>
+        <img src="${resource.src.medium}">
+        `;
+  document.querySelector("body").append(div_dom);
+}
+getPhoto();
