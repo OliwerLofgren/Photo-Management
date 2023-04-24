488f85f8f21074e749a5385188c71b572292bf93
A-me-lie <112852211+A-me-lie@users.noreply.github.com>
2023-04-24 13:50:19 +0200

    Trying to make api to work

--- JS/home.js
+++ JS/home.js
@@ -3,3 +3,5 @@ import { createClient } from 'pexels';
 const client = createClient('4Gkq0ZuJgevDtGOJesppgO5V4tZZ4TZLTBvtO1aX6fgmzxPXGIxmkFg0');
 
 // All requests made with the client will be authenticated
+
+client.photos.show({ id: 2014422 }).then(photo => console.log("picture is ready"));
