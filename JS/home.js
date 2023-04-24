52fc64aca30e65df3ed6b81572bab44d0fb101a1
rabiabee <rabiaahm25@gmail.com>
2023-04-24 14:08:06 +0200

    css file link

---
+++ JS/home.js
@@ -0,0 +1,6 @@
+import { createClient } from 'pexels';
+
+const client = createClient('4Gkq0ZuJgevDtGOJesppgO5V4tZZ4TZLTBvtO1aX6fgmzxPXGIxmkFg0');
+
+// All requests made with the client will be authenticated
+
