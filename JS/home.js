30d10e9c24d7866460461e5ea52ef1eeb6343898
rabiabee <rabiaahm25@gmail.com>
2023-04-24 22:39:33 +0200

    added home page css

--- JS/home.js
+++ JS/home.js
@@ -5,7 +5,7 @@ const mainHome = document.querySelector("main");
 async function getPhoto() {
 
   // NOTE, set per page parameter, + add result object to innerhtml 
-  const per_page = 2;
+  const per_page = 6;
   const url = `${prefix}curated?per_page=${per_page}`;
 
   const response = await fetch_resource(new Request(url, { headers }));
@@ -13,9 +13,14 @@ async function getPhoto() {
 
   const div_dom = document.createElement("div");
 
+  // använd for loop ist? > 
   div_dom.innerHTML = `
       <img src="${resource.photos[0].src.medium}">
       <img src="${resource.photos[1].src.medium}">
+      <img src="${resource.photos[2].src.medium}">
+      <img src="${resource.photos[3].src.medium}">
+      <img src="${resource.photos[4].src.medium}">
+      <img src="${resource.photos[5].src.medium}">
     `;
 
   mainHome.append(div_dom);
@@ -23,4 +28,5 @@ async function getPhoto() {
   console.log(resource);
   console.log(resource.photos[0]);
 }
+
 getPhoto();
