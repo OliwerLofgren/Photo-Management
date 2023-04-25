35b7b97641e5ba9d9b8684cf865919ede52fb6a1
rabiabee <rabiaahm25@gmail.com>
2023-04-25 15:04:16 +0200

    created dom elements for requested photos

--- JS/home.js
+++ JS/home.js
@@ -2,6 +2,8 @@
 
 const mainHome = document.querySelector("main");
 
+renderHomePage();
+
 async function getPhoto() {
 
   // NOTE, set per page parameter, + add result object to innerhtml 
@@ -11,22 +13,30 @@ async function getPhoto() {
   const response = await fetch_resource(new Request(url, { headers }));
   const resource = await response.json();
 
-  const div_dom = document.createElement("div");
+  // create new array from Photo resource, extracting "photos" key 
+  const photosObject = resource.photos;
+  console.log(photosObject);
 
-  // använd for loop ist? > 
-  div_dom.innerHTML = `
-      <img src="${resource.photos[0].src.medium}">
-      <img src="${resource.photos[1].src.medium}">
-      <img src="${resource.photos[2].src.medium}">
-      <img src="${resource.photos[3].src.medium}">
-      <img src="${resource.photos[4].src.medium}">
-      <img src="${resource.photos[5].src.medium}">
+  // create new array based on the photo resource extracting the "photo urls" key
+  const photoUrls = photosObject.map(object => {
+    return object.src.small
+  })
+  console.log(photoUrls);
+
+  const photosWrapper = document.createElement("div");
+
+  // for each photo, create dom element
+  photoUrls.forEach(photo => {
+    const div_dom = document.createElement("div");
+
+    div_dom.innerHTML = `
+      <img src="${photo}">
     `;
-
-  mainHome.append(div_dom);
-
-
-  console.log(resource.photos[0]);
+    photosWrapper.append(div_dom);
+  });
+  mainHome.append(photosWrapper);
 }
 
-getPhoto();
+function renderHomePage() {
+  getPhoto();
+}
