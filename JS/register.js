c2bfec4d927cbad19c03370a89042c79b6ba33aa
rabiabee <rabiaahm25@gmail.com>
2023-04-26 19:13:40 +0200

    Update register.js

--- JS/register.js
+++ JS/register.js
@@ -1,16 +1,31 @@
 "use strict";
 
-document
-
 function renderRegisterPage() {
     mainHome.innerHTML = `
     <h2>Register</h2>
     <p id=message></p>
-    <form>
+    <form id=regForm>
         <input type=text id=username placeholder=Username>
         <input type=password id=password placeholder=Password>
         <button type=submit>Register</button>
     </form>
     <button id=login>Already go an account? Login here</button>
  `;
+
+    const regForm = document.querySelector("#regForm");
+
+    regForm.addEventListener("submit", registerUser)
+}
+
+async function registerUser(event) {
+    event.preventDefault();
+
+    /* oliwer säger kom ihåg att fetcha från mappen register.php */
+
+    try {
+
+    } catch (error) {
+
+    }
+
 }
