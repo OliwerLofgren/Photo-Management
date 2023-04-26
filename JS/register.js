7e32325d7046a6b75d08cee90c477a8454908a02
rabiabee <rabiaahm25@gmail.com>
2023-04-27 00:27:39 +0200

    coded user registration using post method

--- JS/register.js
+++ JS/register.js
@@ -1,7 +1,8 @@
 "use strict";
+// Register
 
-function renderRegisterPage() {
-    mainHome.innerHTML = `
+function createRegisterPage() {
+    main.innerHTML = `
     <h2>Register</h2>
     <p id=message></p>
     <form id=regForm>
@@ -12,20 +13,50 @@ function renderRegisterPage() {
     <button id=login>Already go an account? Login here</button>
  `;
 
+    // redirect to login page if already registered (+ add spicy transitions here)
+    document.querySelector("#login").addEventListener("click", () => {
+        createLoginPage();
+    });
+
+    // register user 
     const regForm = document.querySelector("#regForm");
+    regForm.addEventListener("submit", (event) => {
+        registerUser(event);
+    });
 
-    regForm.addEventListener("submit", registerUser)
-}
 
-async function registerUser(event) {
-    event.preventDefault();
+    async function registerUser(event) {
+        event.preventDefault();
+        /* oliwer säger kom ihåg att fetcha från mappen register.php */
 
-    /* oliwer säger kom ihåg att fetcha från mappen register.php */
+        try {
 
-    try {
+            let response = await fetch_resource("../PHP/register.php");
 
-    } catch (error) {
+            let username = document.querySelector("#username").value;
+            let password = document.querySelector("#password").value;
 
+            const requestBody = {
+                username: username,
+                password: password
+            };
+
+            const post = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(requestBody),
+            };
+
+            let resource = await fetch("/PHP/register.php", post);
+            let data = await resource.json();
+
+            // display success message to the user
+            const message = document.querySelector("#message");
+            message.textContent = "Registration successful!";
+
+        } catch (error) {
+            console.log("oops");
+        }
     }
-
 }
+
