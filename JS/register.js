d8251bb9405416b473af941697b83fb27c7e62aa
A-me-lie <112852211+A-me-lie@users.noreply.github.com>
2023-04-26 13:45:44 +0200

    form register

---
+++ JS/register.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function renderLoginPage() {
+    mainHome.innerHTML = `
+    <h2>Register</h2>
+    <p id=message></p>
+    <form>
+        <input type=text id=username placeholder=Username>
+        <input type=password id=password placeholder=Password>
+        <button type=submit>Register</button>
+    </form>
+    <button id=login>Already go an account? Login here</button>
+ `;
+}
