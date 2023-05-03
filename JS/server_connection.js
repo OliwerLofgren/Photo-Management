568513650cbd92a8b655c0e40f8a3007a5eacba1
Oliwer <oliwerl@outlook.com>
2023-05-03 09:15:39 +0200

    aa

---
+++ JS/server_connection.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const prefix = "https://api.pexels.com/v1/";
+
+const api_key = "d7eBBdpVdN08nChtJhFZzudXealrUpI6Xz0FsfuK0d5klpBSt6XzL2Zm";
+
+const headers = {
+  authorization: api_key,
+};
+
+function fetch_resource(request) {
+  return fetch(request);
+}
+
+// display server message (NOTE: temporary solution)
+function displayServerMessage(response) {
+  const message = document.createElement("p");
+  console.log(response);
+  message.innerHTML = `
+        <p>${response.statusText}</p>
+    `;
+  document.querySelector("main").appendChild(message);
+}
