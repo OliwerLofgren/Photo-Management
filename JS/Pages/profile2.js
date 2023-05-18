3a0a53c04159712ac3b94c37b6c530b81d6c43a5
A-me-lie <112852211+A-me-lie@users.noreply.github.com>
2023-05-18 15:56:50 +0200

    edits

--- JS/Pages/profile2.js
+++ JS/Pages/profile2.js
@@ -261,17 +261,6 @@ async function createProfileGalleryPage(user) {
                 target_element.innerHTML = "";
                 target_element.appendChild(img);
             } else {
-                // Check if default profile picture URL is stored in localStorage
-                let defaultImageUrl = localStorage.getItem("defaultProfilePic");
-
-                if (!defaultImageUrl) {
-                    // If default profile picture URL is not stored, set it to the local file path
-                    defaultImageUrl = "./media/default-profile.png";
-
-                    // Store the default profile picture URL in localStorage
-                    localStorage.setItem("defaultProfilePic", defaultImageUrl);
-                }
-
                 const icon = document.createElement("i");
                 icon.className = "fa-solid fa-user";
                 icon.id = "userIcon"
