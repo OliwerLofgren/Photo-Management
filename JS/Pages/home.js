ffd0d64be49a71d84ba7fae965b8472c4b446e25
A-me-lie <112852211+A-me-lie@users.noreply.github.com>
2023-05-24 17:49:24 +0200

    edits

--- JS/Pages/home.js
+++ JS/Pages/home.js
@@ -52,9 +52,9 @@ function createHomePage() {
 
     <section id="home-section-four" class="section">
       <h1 class="h1-four">Collect your favorites so you can get back to them later</h1>
-
-      <div class="circle-home">
-          <p class="circle-text">Be inspired</p>
+      <div class="bookmarkHome">
+      <i class="fa-regular fa-bookmark" id="markHome" style="color: #000000;"></i>
+      <i class="fa-solid fa-bookmark" id="markHome" style="color: #f50000;"></i>
       </div>
     </section>
 
@@ -117,5 +117,7 @@ function createHomePage() {
 //         <div class="alt">Fire</div>
 //         <div class="alt">Mountain</div>
 //         </div>
-//<i class="fa-solid fa-bookmark" style="color: #f50000;"></i>
-//<i class="fa-regular fa-bookmark" style="color: #000000;"></i>
+
+//<div class="circle-home">
+// <p class="circle-text">Be inspired</p>
+//</div> 
