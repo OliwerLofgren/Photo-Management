f5dc54e6b8aeb3b9af7df1964db28f6cdc47e0f7
Oliwer <oliwerl@outlook.com>
2023-05-25 18:43:03 +0200

    ...

--- JS/Pages/home.js
+++ JS/Pages/home.js
@@ -113,4 +113,3 @@ function createHomePage() {
       .addEventListener("click", createRegisterPage);
   }
 }
-
--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,9 @@
 "use strict";
 let user = null;
 user = initializeObject("user"); // initialize the user
-
+console.log(user);
 if (user) {
+  console.log(user);
   createDiscoverPage(user);
 } else {
   createHomePage();
