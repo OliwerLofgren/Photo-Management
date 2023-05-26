3194d9b0b4d48be4ae292dcd386ae19065699447
rabiabee <rabiaahm25@gmail.com>
2023-05-26 14:11:37 +0200

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -2,12 +2,11 @@
 ini_set("display_errors", 1);
 require_once("functions.php");
 
-$filename = "Photo-Management/JSON/users.json";
+$filename = "../Photo-Management/JSON/users.json";
 $users = [];
 
 $users = json_decode(file_get_contents($filename), true);
 $input_data = json_decode(file_get_contents("php://input"), true);
-sendJSON($users);
 
 $request_method = $_SERVER["REQUEST_METHOD"];
 //Check if method is POST otherwise error message
